17135ac338dca713c92d24905981430b9412ec1b
Xuncheng Liu <liuxunchenglxc@163.com>
2023-09-01 01:17:22 +0800

    add pydoc by sphinx

---
+++ docs/conf.py
@@ -0,0 +1,51 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'CompilerFramework-Python'
+copyright = '2023, 刘迅承'
+author = '刘迅承'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.todo',
+    'sphinx.ext.napoleon'
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'en'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+#html_static_path = ['_static']
+
+# -- Options for todo extension ----------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/todo.html#configuration
+
+todo_include_todos = True
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))
+def skip(app, what, name, obj, would_skip, options):
+    if name in ( '__init__',):
+        return False
+    return would_skip
+def setup(app):
+    app.connect('autodoc-skip-member', skip)
+extensions.append('sphinx_autodoc_typehints')
+extensions.append('myst_parser')
+extensions.append('sphinx_markdown_builder')
+
